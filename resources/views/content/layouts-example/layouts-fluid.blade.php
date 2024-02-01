c3b4f55b686b7caddeb266c475bec0af8c706b8d
RenanMontenegro2 <126257350+RenanMontenegro2@users.noreply.github.com>
2024-02-01 16:49:03 -0300

    feat:"Criação da Controller + Request + Model + Migration de Reino"

--- resources/views/content/layouts-example/layouts-fluid.blade.php
+++ resources/views/content/layouts-example/layouts-fluid.blade.php
@@ -9,9 +9,6 @@
 @section('content')
 <!-- Layout Demo -->
 <div class="layout-demo-wrapper">
-  <div class="layout-demo-placeholder">
-    <img src="{{asset('assets/img/layouts/layout-fluid-light.png')}}" class="img-fluid" alt="Layout fluid">
-  </div>
   <div class="layout-demo-info">
     <h4>Layout fluid</h4>
     <p>Fluid layout sets a <code>100% width</code> at each responsive breakpoint.</p>
